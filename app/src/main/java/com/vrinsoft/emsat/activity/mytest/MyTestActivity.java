6ccb7d04c4d043c7c4174ca26d2c21c7bab69d8a
Specter <yasmik.vrinsoft@gmail.com>
2018-02-26 12:57:40 +0530

    My TestList UI done

---
+++ app/src/main/java/com/vrinsoft/emsat/activity/mytest/MyTestActivity.java
@@ -0,0 +1,138 @@
+package com.vrinsoft.emsat.activity.mytest;
+
+import android.app.Activity;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+
+import com.vrinsoft.emsat.MasterActivity;
+import com.vrinsoft.emsat.R;
+import com.vrinsoft.emsat.activity.subcategory.model.BeanNotificationList;
+import com.vrinsoft.emsat.activity.subcategory.model.Result;
+import com.vrinsoft.emsat.apis.api.subcategory_list.OnSubCatList;
+import com.vrinsoft.emsat.apis.api.subcategory_list.SubCategoryListApiHandler;
+import com.vrinsoft.emsat.databinding.ActivityMytestBinding;
+import com.vrinsoft.emsat.robinhood.router.Director;
+import com.vrinsoft.emsat.utils.AppConstants;
+import com.vrinsoft.emsat.utils.AppPreference;
+import com.vrinsoft.emsat.utils.NavigationUtils;
+import com.vrinsoft.emsat.utils.Pref;
+import com.vrinsoft.emsat.utils.ViewUtils;
+
+import java.util.ArrayList;
+
+
+public class MyTestActivity extends MasterActivity {
+    private static int PERCENT = 0;
+    String mTitle = "";
+    ActivityMytestBinding binding;
+    Activity mActivity;
+    ArrayList<Result> mArrayList = new ArrayList<>();
+    MyTestListAdapter mAdapter;
+    private SubCategoryListApiHandler subCategoryListApiHandler;
+    private LinearLayoutManager linearLayoutManager;
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public View getContentLayout() {
+        binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.activity_mytest, null, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+        director = new Director(this);
+        subCategoryListApiHandler = new SubCategoryListApiHandler();
+        setUIConfig();
+        fetchNotificationList(1);
+    }
+
+    private void setUIConfig() {
+        binding.rvSubCategory.setNestedScrollingEnabled(false);
+        linearLayoutManager = new LinearLayoutManager(mActivity, LinearLayoutManager.VERTICAL, false);
+        binding.rvSubCategory.setLayoutManager(linearLayoutManager);
+        mAdapter = new MyTestListAdapter(mActivity, mArrayList, new MyTestListAdapter.OnClickable() {
+            @Override
+            public void getPosition(int position) {
+                ViewUtils.showToast(mActivity, ""+position, null);
+            }
+        });
+        binding.rvSubCategory.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null && bundle.containsKey(AppConstants.INTENT_NAME)) {
+            mTitle = bundle.getString(AppConstants.INTENT_NAME);
+        }
+        setToolBarConfig();
+    }
+
+    public void setToolBarConfig() {
+        masterBinding.toolbar.txtTitle.setVisibility(View.VISIBLE);
+        masterBinding.toolbar.txtTitle.setText(mTitle);
+        masterBinding.toolbar.imgHome.setVisibility(View.GONE);
+        masterBinding.toolbar.imgBack.setVisibility(View.VISIBLE);
+        masterBinding.toolbar.rlNotification.setVisibility(View.GONE);
+        masterBinding.toolbar.imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavigationUtils.finishCurrentActivity(mActivity);
+            }
+        });
+    }
+
+    private void fetchNotificationList(int pageNo) {
+
+        ViewUtils.showDialog(mActivity, false);
+        subCategoryListApiHandler.fetchSubCatData(Pref.getValue(mActivity, AppPreference.USER_INFO.PREF_USER_ID, ""),
+                Pref.getValue(mActivity, AppPreference.USER_INFO.PREF_TOKEN, ""),
+                pageNo, Pref.getValue(mActivity, AppPreference.USER_INFO.PREF_USER_TYPE, 0), new OnSubCatList() {
+                    @Override
+                    public void getResponse(boolean isSuccess, ArrayList<BeanNotificationList> beanNotificationList, String errorMsgSystem) {
+                        ViewUtils.showDialog(mActivity, true);
+                        if (isSuccess) {
+                            if (beanNotificationList.get(0).getCode() == AppConstants.API_CODE_RESPONSE_SUCCESS) {
+                                mArrayList.clear();
+                                mArrayList.addAll(beanNotificationList.get(0).getResult());
+                                if (mArrayList.size() > 0) {
+                                    binding.txtNoDataFound.setVisibility(View.GONE);
+                                    binding.rvSubCategory.setVisibility(View.VISIBLE);
+                                    mAdapter.notifyDataSetChanged();
+                                } else {
+                                    binding.txtNoDataFound.setVisibility(View.VISIBLE);
+                                    binding.rvSubCategory.setVisibility(View.GONE);
+                                }
+                            } else {
+                                if (AppConstants.isTestModeOn) {
+                                    mArrayList.clear();
+                                    for (int i = 1; i <= 4; i++) {
+                                        Result result = new Result();
+                                        result.setBroadcastMsg("Topic " + i);
+                                        mArrayList.add(result);
+                                    }
+                                    mAdapter.notifyDataSetChanged();
+                                    binding.txtNoDataFound.setVisibility(View.GONE);
+                                    binding.rvSubCategory.setVisibility(View.VISIBLE);
+                                } else {
+                                    binding.txtNoDataFound.setVisibility(View.VISIBLE);
+                                    binding.rvSubCategory.setVisibility(View.GONE);
+                                }
+                            }
+                        } else {
+                            ViewUtils.showToast(mActivity, errorMsgSystem, null);
+                        }
+                    }
+                });
+    }
+
+}
---
+++ app/src/main/java/com/vrinsoft/emsat/activity/mytest/MyTestListAdapter.java
@@ -0,0 +1,92 @@
+package com.vrinsoft.emsat.activity.mytest;
+
+
+import android.app.Activity;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daimajia.swipe.SwipeLayout;
+import com.vrinsoft.emsat.R;
+import com.vrinsoft.emsat.activity.subcategory.model.Result;
+import com.vrinsoft.emsat.databinding.CustomRowMytestBinding;
+
+import java.util.ArrayList;
+
+
+public class MyTestListAdapter extends RecyclerView.Adapter<MyTestListAdapter.ViewHolder> {
+
+    Activity mActivity;
+    ArrayList<Result> mArrayList;
+    OnClickable onClickable;
+    public interface OnClickable
+    {
+        public void getPosition(int position);
+    }
+    public MyTestListAdapter(Activity mActivity, ArrayList<Result> mArrayList, OnClickable onClickable) {
+        this.mActivity = mActivity;
+        this.mArrayList = mArrayList;
+        this.onClickable = onClickable;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CustomRowMytestBinding mBinding = DataBindingUtil.inflate
+                (LayoutInflater.from(mActivity),
+                        R.layout.custom_row_mytest, parent, false);
+        return new ViewHolder(mBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position)
+    {
+        holder.mBinding.swipe.setClickToClose(true);
+        holder.mBinding.txtProgress.setText(""+Math.round(holder.mBinding.progressScore.getPercent())+"/"+100);
+        holder.mBinding.txtCategory.setText(getArrayList().get(position).getBroadcastMsg());
+        holder.mBinding.txtSubCategory.setText(getArrayList().get(position).getBroadcastMsg());
+        holder.mBinding.txtEndDate.setText("10 Jan, 2018");
+        holder.mBinding.swipe.setTag(position);
+        holder.mBinding.llMain.setTag(holder.mBinding.swipe);
+        holder.mBinding.llRetakeQuiz.setTag(holder.mBinding.swipe);
+        holder.mBinding.llRetakeQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwipeLayout swipeLayout = ((SwipeLayout) v.getTag());
+                onClickable.getPosition((Integer) swipeLayout.getTag());
+                swipeLayout.toggle(true);
+            }
+        });
+
+        holder.mBinding.llMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SwipeLayout swipeLayout = ((SwipeLayout) v.getTag());
+                swipeLayout.toggle(true);
+            }
+        });
+    }
+
+    public ArrayList<Result> getArrayList() {
+        return mArrayList;
+    }
+
+    public void add(Result item) {
+        mArrayList.add(item);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CustomRowMytestBinding mBinding;
+        public ViewHolder(CustomRowMytestBinding itemNotificationListBinding) {
+            super(itemNotificationListBinding.getRoot());
+            mBinding = itemNotificationListBinding;
+        }
+    }
+}

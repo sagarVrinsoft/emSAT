d0aedf0446d8a17f87b7b1a2d2b7753e366b41c6
Specter <sagar.vrinsofts@gmail.com>
2018-02-28 17:19:19 +0530

    ViewProfile Api Integrate

--- app/src/main/java/com/vrinsoft/emsat/utils/AppConstants.java
+++ app/src/main/java/com/vrinsoft/emsat/utils/AppConstants.java
@@ -211,4 +211,11 @@ public class AppConstants {
 //        public static final int THREE = 3;
 //        public static final int FOUR = 4;
     }
+
+    /*NUM_OF_CHECKED_VIEWS indicates no. of views to be checked/unchecked*/
+    public interface NUM_OF_CHECKED_VIEWS {
+        public static final int TWO = 2;
+//        public static final int THREE = 3;
+//        public static final int FOUR = 4;
+    }
 }
